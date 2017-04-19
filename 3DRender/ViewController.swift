b3f165f99b5ab49256f3f8e1f5fe6bd78ab0feeb
Reda Mimouni <rmimouni@octo.com>
2017-04-19 10:07:59 +0200

    Initial Commit

---
+++ 3DRender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3DRender
+//
+//  Created by Reda Mimouni on 19/04/2017.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
