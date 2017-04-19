1fb8d10c8609e090e0ab53422cbcd4cf91ad9812
Reda Mimouni <rmimouni@octo.com>
2017-04-19 11:29:23 +0200

    Add mesh and camera objects to controller

--- 3DRender/ViewController.swift
+++ 3DRender/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 import NinevehGL
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NGLViewDelegate{
+  
+  var mesh = NGLMesh()
+  var camera = NGLCamera()
+  
+  /*!
+   *					Method called at each render cycle.
+   *
+   *					Place all your drawing commands inside this method. NinevehGL drawing is done by using
+   *					cameras. For each camera you want to draw, call its <code>#drawView#</code> method.
+   *					Each camera called inside this method will produce its resulting image into the
+   *					correspondent NGLView.
+   */
+  func drawView() {
+    
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view, typically from a nib.
+    mesh = NGLMesh(file: "back_flip_to_uppercut.dae", settings: [kNGLMeshCentralizeYes: kNGLMeshKeyCentralize, "0.3": kNGLMeshKeyNormalize], delegate: nil)
+    camera.add(mesh)
+    camera.autoAdjustAspectRatio(true, animated: true)
   }
 
   override func didReceiveMemoryWarning() {
